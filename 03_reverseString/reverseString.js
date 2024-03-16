63a21ac2177b1f3484541110eb44a5b87aa9f830
NatanBoz <natan.bozydaj@gmail.com>
2024-03-16 15:41:47 +0100

    Finish exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let result = [];
+    let i = 0
+    do {
+        result.push(str.at(i));
+        i++;
+    } while (i < str.length)
+    const reversedArray = result.reverse();
+    return reversedArray.join("");
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array = [], ...remove) {
+    const filteredArray = array.filter(item => !remove.includes(item));
+    return filteredArray
 };
 
 // Do not edit below this line
