e399b4b81d1bed8b28c1862ad1cf73b32801068e
NatanBoz <natan.bozydaj@gmail.com>
2024-03-18 12:27:50 +0100

    Solve next exercises number 6 and 7

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,20 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let condition1 = function() {
+        if (year % 4 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    let condition2 = function() {
+        if (year % 100 == 0 && !(year % 400 == 0 || year % 800 == 0)) {
+            return false
+        } else {
+            return true
+        }    
+    }
+    let result = condition1() && condition2()
+    return Boolean(result)
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp1) {
+  let result = (temp1 - 32) * 5/9;
+  return parseFloat(result.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp2) {
+  let result = (temp2 * (9/5) +32);
+  return parseFloat(result.toFixed(1));
 };
 
 // Do not edit below this line
